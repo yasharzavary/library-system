af5c271816e811a16e53f335d35caa5b3b53777b
yashar <115088770+yasharzavary@users.noreply.github.com>
2023-04-04 17:15:47 +0430

    add key's input and output is ready...now push key request

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from mysql.connector import Connect, Error
 
 # my main root
 mainroot=Tk()
+mainroot.resizable(width=0,height=0)
 
 w=300
 h=300
@@ -15,6 +16,23 @@ x=(sw/2)-(w/2)
 y=(sh/2)-(h/2)
 
 mainroot.geometry("%dx%d+%d+%d"%(w,h,x,y))
+
+
+# add key button
+def ecchangecolor(event):
+    addkey.config(bg="#DCDCDC")
+
+def lchangekey(event):
+    addkey.config(bg="#ffffff")
+    
+def pushkey(event):
+    pass
+    
+addkey=Button(master=mainroot, text="new book", fg="#000000", bg="#ffffff",width=10)
+addkey.bind("<Enter>",ecchangecolor)
+addkey.bind("<Leave>",lchangekey)
+addkey.bind("<Button>",pushkey)
+addkey.grid(row=0,column=0, padx=100)
 
 
 mainroot.mainloop()
